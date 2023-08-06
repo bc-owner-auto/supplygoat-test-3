ba546e8c695d45748e618dd29b3045b2e38874b8
bc-owner-auto <110401622+bc-owner-auto@users.noreply.github.com>
2023-08-06 17:37:35 +0300

    Initialize alonTest3Branch

---
+++ alonTest3.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "${local.resource_prefix.value}-analysis-lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+  tags = {
+    git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
+    git_file             = "terraform/aws/lambda.tf"
+    git_last_modified_at = "2020-06-16 14:46:24"
+    git_last_modified_by = "nimrodkor@gmail.com"
+    git_modifiers        = "nimrodkor"
+    git_org              = "bridgecrewio"
+    git_repo             = "terragoat"
+    yor_trace            = "93cfa6f9-a257-40c3-b7dc-3c3686929734"
+  }
+}
